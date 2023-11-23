fc4aa36687e41fcddb646e2fb5799b083a765b36
Allen Rudolph <55750919+upitroma@users.noreply.github.com>
2023-11-22 22:43:19 -0500

    Create wordle.sh

---
+++ wordle.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Wordle API URL
+API_URL="https://www.nytimes.com/svc/wordle/v2/$(date +'%Y-%m-%d').json"
+
+# Fetch Wordle data
+wordle_data=$(curl -s "$API_URL")
+
+# Extract the target word
+target_word=$(echo "$wordle_data" | jq -r '.solution')
+
+#echo "For added security, please enter today's Wordle answer:"
+
+attempts=0
+
+while true; do
+	read -p "For added security, please enter today's Wordle answer (lowercase): " user_guess
+
+    # Check if the guess is correct
+    if [ "$user_guess" == "$target_word" ]; then
+        break
+    else
+	echo "Wrong! Try again!"
+    fi
+done
+
+# Grant shell access
+echo "Access granted. Welcome to the shell!"
+/bin/bash
